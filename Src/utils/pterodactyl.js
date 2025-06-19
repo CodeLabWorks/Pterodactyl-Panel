8c3cc75653a0cd8b18eca3c57559d1f3772aee1f
Shadow Gaming <81495140+ShadowGaming100@users.noreply.github.com>
2025-06-19 12:43:50 +0100

    Update pterodactyl.js

--- Src/utils/pterodactyl.js
+++ Src/utils/pterodactyl.js
@@ -1,17 +1,11 @@
-
 const axios = require('axios');
 const config = {
   panelURL: process.env.PTERO_PANEL_URL,
   apiKey: process.env.PTERO_API_KEY,
 };
 
-// Ensure panelURL uses HTTPS
-const sanitizedURL = config.panelURL.startsWith('http')
-  ? config.panelURL.replace(/^http:/, 'https:')
-  : `https://${config.panelURL}`;
-
 const api = axios.create({
-  baseURL: `${sanitizedURL}/api/client`,
+  baseURL: `${config.panelURL}/api/client`,
   headers: {
     Authorization: `Bearer ${config.apiKey}`,
     'Content-Type': 'application/json',
